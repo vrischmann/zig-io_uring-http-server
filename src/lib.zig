47b8bbaf4c96803407541607611df8b2c1fcc700
Vincent Rischmann <vincent@rischmann.fr>
2021-12-26 00:09:52 +0100

    add a Method type

--- src/lib.zig
+++ src/lib.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const ascii = std.ascii;
 const debug = std.debug;
 const fmt = std.fmt;
 const heap = std.heap;
@@ -29,7 +30,57 @@ const c = @cImport({
     @cInclude("picohttpparser.h");
 });
 
-pub const StatusCode = enum(u16) {
+pub const Method = enum(u4) {
+    get,
+    head,
+    post,
+    put,
+    delete,
+    connect,
+    options,
+    trace,
+    patch,
+
+    pub fn toString(self: Method) []const u8 {
+        switch (self) {
+            .get => return "GET",
+            .head => return "HEAD",
+            .post => return "POST",
+            .put => return "PUT",
+            .delete => return "DELETE",
+            .connect => return "CONNECT",
+            .options => return "OPTIONS",
+            .trace => return "TRACE",
+            .patch => return "PATCH",
+        }
+    }
+
+    fn fromString(s: []const u8) !Method {
+        if (ascii.eqlIgnoreCase(s, "GET")) {
+            return .get;
+        } else if (ascii.eqlIgnoreCase(s, "HEAD")) {
+            return .head;
+        } else if (ascii.eqlIgnoreCase(s, "POST")) {
+            return .post;
+        } else if (ascii.eqlIgnoreCase(s, "PUT")) {
+            return .put;
+        } else if (ascii.eqlIgnoreCase(s, "DELETE")) {
+            return .delete;
+        } else if (ascii.eqlIgnoreCase(s, "CONNECT")) {
+            return .connect;
+        } else if (ascii.eqlIgnoreCase(s, "OPTIONS")) {
+            return .options;
+        } else if (ascii.eqlIgnoreCase(s, "TRACE")) {
+            return .trace;
+        } else if (ascii.eqlIgnoreCase(s, "PATCH")) {
+            return .patch;
+        } else {
+            return error.InvalidMethod;
+        }
+    }
+};
+
+pub const StatusCode = enum(u10) {
     // informational
     continue_ = 100,
     switching_protocols = 101,
