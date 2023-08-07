12ba3af1730791cf0d620be78f69db10a94455c5
Vincent Rischmann <vincent@rischmann.fr>
2023-08-07 23:02:12 +0200

    build: fix

--- build.zig
+++ build.zig
@@ -22,7 +22,10 @@ pub fn build(b: *std.build.Builder) void {
         .target = target,
         .optimize = optimize,
     });
-    picohttp.addCSourceFile("src/picohttpparser.c", picohttp_flags);
+    picohttp.addCSourceFile(.{
+        .file = .{ .path = "src/picohttpparser.c" },
+        .flags = picohttp_flags,
+    });
     picohttp.linkLibC();
 
     const build_options = b.addOptions();
@@ -38,7 +41,7 @@ pub fn build(b: *std.build.Builder) void {
     exe.addAnonymousModule("args", .{
         .source_file = .{ .path = "third_party/zig-args/args.zig" },
     });
-    exe.addIncludePath("src");
+    exe.addIncludePath(.{ .path = "src" });
     exe.linkLibrary(picohttp);
     exe.addOptions("build_options", build_options);
     b.installArtifact(exe);
@@ -49,7 +52,7 @@ pub fn build(b: *std.build.Builder) void {
         .target = target,
         .optimize = optimize,
     });
-    tests.addIncludePath("src");
+    tests.addIncludePath(.{ .path = "src" });
     tests.linkSystemLibrary("curl");
     tests.linkLibrary(picohttp);
     tests.addOptions("build_options", build_options);
